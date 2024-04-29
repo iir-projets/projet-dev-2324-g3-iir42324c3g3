d7af83e1d07f34ce83c678f9822c818f2eb8885a
abdelmoughiterrabih <moughit3000@gmail.com>
2024-04-29 14:53:20 +0200

    creation d interface gestion Reservation
    table de reservation

--- front_admin/src/Pages/Reservation/index.js
+++ front_admin/src/Pages/Reservation/index.js
@@ -1,13 +1,164 @@
-import React from 'react';
-import { Typography } from 'antd';
-const { Title } = Typography;
-const Reservation = () => (
-  <>
-    <Title>h1. Ant Design</Title>
-    <Title level={2}>h2. Ant Design</Title>
-    <Title level={3}>h3. Ant Design</Title>
-    <Title level={4}>h4. Ant Design</Title>
-    <Title level={5}>h5. Ant Design</Title>
-  </>
-);
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import { Table, Pagination, Space, Button, Input, message } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { useNavigate  } from 'react-router-dom'; 
+
+
+const showTotal = (total) => `Total ${total} items`;
+
+const Reservation= () => {
+  const [current, setCurrent] = useState(1);
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+ 
+  const navigate = useNavigate(); 
+
+  
+
+  
+
+  const onChange = (page) => {
+    setCurrent(page);
+  };
+
+  const onSearch = (value) => {
+    setSearchQuery(value);
+  };
+
+  const filteredData = data.filter((item) => {
+    return (
+      item.id.toString().includes(searchQuery) ||
+      item.Titre.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  });
+
+  const getColumnSearchProps = (dataIndex, title) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder={`Search ${title}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => confirm()}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => confirm()}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => clearFilters()} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+        : '',
+  });
+
+  
+
+  const handleAddReservation = () => {
+    
+  };
+ 
+  const modifierReservation = (id) => {
+    
+  };
+  
+  
+  const voirReservation = (id) => {
+   
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      sorter: (a, b) => a.id - b.id,
+      ...getColumnSearchProps('id', 'ID'),
+    },
+    {
+      title: 'Titre',
+      dataIndex: 'Titre',
+      key: 'Titre',
+      render: (text) => <a>{text}</a>,
+      ...getColumnSearchProps('Titre', 'Titre'),
+    },
+    {
+      title: 'Ville',
+      dataIndex: 'Ville',
+      key: 'Ville',
+      ...getColumnSearchProps('Ville', 'Ville'),
+    },
+    {
+      title: 'Horaire',
+      dataIndex: 'Horaire',
+      key: 'Horaire',
+      ...getColumnSearchProps('Horaire', 'Horaire'),
+    },
+    {
+      title: 'Frais',
+      dataIndex: 'Frais',
+      key: 'Frais',
+      ...getColumnSearchProps('Frais', 'Frais'),
+    },
+    
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <Button onClick={() => voirReservation(record.id)}>Voir</Button>
+          <Button type="primary" onClick={() => modifierReservation(record.id)}>Modifier</Button>
+          <Button danger >
+            Supprimer
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <h1>Gestion Rerservation</h1>
+
+      <div className="searchContainer">
+        <Input
+          placeholder="Rechercher une Reservation"
+          className="searchInput"
+          onChange={(e) => onSearch(e.target.value)}
+        />
+        <Button type="primary" icon={<SearchOutlined />} className="searchButton"></Button>
+        <Button type="primary" className="addButton leftButton" onClick={handleAddReservation}>
+          Ajouter une Reservation
+        </Button>
+      </div>
+
+      <Table
+        columns={columns}
+        dataSource={filteredData}
+        pagination={{
+          current: current,
+          pageSize: 5,
+          total: filteredData.length,
+          onChange: onChange,
+          showTotal: showTotal,
+        }}
+      />
+    </>
+  );
+};
+
 export default Reservation;
