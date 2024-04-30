80a87d2e38c166800eec0a0cf2a0a907bcb4967a
abdelmoughiterrabih <moughit3000@gmail.com>
2024-04-30 22:30:59 +0200

    interface ajouterPassageret modifierPassager
    gadithoum bjouj

--- front_admin/src/Pages/Passager/ajouterPassager/index.js
+++ front_admin/src/Pages/Passager/ajouterPassager/index.js
@@ -1,13 +1,107 @@
 import React from 'react';
-import { Typography } from 'antd';
-const { Title } = Typography;
-const AjouterPassager = () => (
-  <>
-    <Title>h1. Ant Design</Title>
-    <Title level={2}>h2. Ant Design</Title>
-    <Title level={3}>h3. Ant Design</Title>
-    <Title level={4}>h4. Ant Design</Title>
-    <Title level={5}>h5. Ant Design</Title>
-  </>
-);
+import { Button, Form, Input, Space } from 'antd';
+
+const AjouterPassager = () => {
+  const [form] = Form.useForm();
+
+  const SubmitButton = ({ children }) => {
+    const [submittable, setSubmittable] = React.useState(false);
+
+    // Watch all values
+    const values = Form.useWatch([], form);
+    React.useEffect(() => {
+      form
+        .validateFields({
+          validateOnly: true,
+        })
+        .then(() => setSubmittable(true))
+        .catch(() => setSubmittable(false));
+    }, [form, values]);
+
+    return (
+      <Button type="primary" htmlType="submit" disabled={!submittable}>
+        {children}
+      </Button>
+    );
+  };
+
+  return (
+    <Form form={form} name="validateOnly" layout="vertical" autoComplete="off">
+      <Form.Item
+        name="cin"
+        label="CIN"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="nomPassager"
+        label="Nom du Passager"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="prenomPassager"
+        label="Prénom du Passager"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="emailPassager"
+        label="Email du Passager"
+        rules={[
+          {
+            required: true,
+            type: 'email',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="adressePassager"
+        label="Adresse du Passager"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="telephonePassager"
+        label="Téléphone du Passager"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Space>
+          <SubmitButton>Ajouter</SubmitButton>
+          <Button htmlType="reset">Reset</Button>
+        </Space>
+      </Form.Item>
+    </Form>
+  );
+};
+
 export default AjouterPassager;
