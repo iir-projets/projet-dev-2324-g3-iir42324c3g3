28f96bd68b05167d19fdd53ccf7e329d18c41cb2
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-05-01 16:09:19 +0100

    remove backend

--- gestion_companies_aeriennes_backend/src/main/java/com/example/gestion_companies_aeriennes_backend/DAO/PassagerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestion_companies_aeriennes_backend.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.gestion_companies_aeriennes_backend.model.Passager;
-
-public interface PassagerRepository extends JpaRepository<Passager,String> {
-
-}
--- gestion_companies_aeriennes_backend/src/main/java/com/example/gestion_companies_aeriennes_backend/DAO/PaysRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gestion_companies_aeriennes_backend.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.gestion_companies_aeriennes_backend.model.Pays;
-public interface PaysRepository extends JpaRepository<Pays,Integer> {
-
-}
